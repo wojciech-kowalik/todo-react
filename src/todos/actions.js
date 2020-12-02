99cac0f9c997e3b9327d0470e895c8ff925e2e7d
Wojciech Kowalik <wojciech.kowalik@visualnet.pl>
2020-12-02 21:22:36 +0100

    Initial commit

---
+++ src/todos/actions.js
@@ -0,0 +1,20 @@
+// action types
+export const CREATE_TODO = 'CREATE_TODO';
+export const REMOVE_TODO = 'REMOVE_TODO';
+export const MARK_TODO_AS_COMPLETED = 'MARK_TODO_AS_COMPLETED';
+
+// action creators
+export const createTodo = text => ({
+    type: CREATE_TODO,
+    payload: { text }
+});
+
+export const removeTodo = text => ({
+    type: REMOVE_TODO,
+    payload: { text }
+});
+
+export const markTodoAsCompleted = text => ({
+    type: MARK_TODO_AS_COMPLETED,
+    payload: { text },
+});
