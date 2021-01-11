2e2a4d74e1e565c7174c4b3d65ee931f7e69ce76
Wojciech Kowalik <wojciech.kowalik@visualnet.pl>
2021-01-11 15:23:56 +0100

    Styled components instead of css modules

--- src/components/todos/TodoList.js
+++ src/components/todos/TodoList.js
@@ -1,5 +1,7 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
+import styled from 'styled-components';
+
 import NewTodoForm from './NewTodoForm';
 import TodoListItem from './TodoListItem';
 import { 
@@ -13,7 +15,11 @@ import {
     getCompleteTodos,
     getIncompleteTodos
 } from '../../data/selectors'
-import './TodoList.css';
+
+const ListWrapper = styled.div`
+    max-width: 700px;
+    margin: auto;
+`;
 
 const TodoList = ({ 
     inCompleteTodos = [], 
@@ -31,7 +37,7 @@ const TodoList = ({
     const loadingMessage = <div>Loading todos ...</div>;
 
     const content =  (
-        <div className="list-wrapper">
+        <ListWrapper>
             <NewTodoForm />
             <h3>Incomplete:</h3>
             {inCompleteTodos.map((todo, index) => <TodoListItem
@@ -45,7 +51,7 @@ const TodoList = ({
                 todo={todo}
                 onRemovePressed={onRemovePressed}
                 onCompletedPressed={onCompletedPressed}/>)}    
-        </div>
+        </ListWrapper>
     )
 
     return isLoading ? loadingMessage : content;
